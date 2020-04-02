96b9c18a12424488a7c17a856fde7c50ab44ba82
xZugs <62754799+xZugs@users.noreply.github.com>
2020-04-02 11:57:55 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,21 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var valuesAleat = [1, undefined, true, null , "junior"];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+var myFunction = function(array){
+return array;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+console.log(myFunction(valuesAleat)[1]);
+
+//undefined
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +24,33 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+var myFunction2 = function(array, a){
+return array[a];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var valuesDif = ["Jason", true, [1, 2, 3], { carro: "Lambo"} , 9.9];
+
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+
+console.log (myFunction2(valuesDif, 0))
+console.log (myFunction2(valuesDif, 1))
+console.log (myFunction2(valuesDif, 2))
+console.log (myFunction2(valuesDif, 3))
+console.log (myFunction2(valuesDif, 4))
+/*
+Jason
+true
+[ 1, 2, 3 ]       
+{ carro: 'Lambo' }
+9.9
+*/
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +66,58 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+var book = function( livroNome ){
+var allLivros = { 
+        "Skyrim": {
+        quantidadePaginas: 500,
+        autor: "Dovhakin",
+        editora: "Bethesda"
+        },
+        "COD": {
+        quantidadePaginas: 300,
+        autor: "Price",
+        editora: "infinite War"
+        },
+        "GOD OF WAR": {
+        quantidadePaginas: 600,
+        autor: "Matthew Stover",
+        editora: "Leya"
+    }
+};
+
+return !livroNome ? allLivros : allLivros[ livroNome ];
+
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+console.log(book());
+/*{Skyrim: {…}, COD: {…}, GOD OF WAR: {…}}
+Skyrim: {quantidadePaginas: 500, autor: "Dovhakin", editora: "Bethesda"}
+COD: {quantidadePaginas: 300, autor: "Prince", editora: "infinite War"}
+GOD OF WAR: {quantidadePaginas: 600, autor: "Matthew Stover", editora: "Leya"}
+__proto__: Object
+*/
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+var livro = "COD"
+console.log("O livro " + livro +" tem "+ book("COD").quantidadePaginas + " páginas!");
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+console.log("O autor do livro " + livro + " é " + book( livro ).autor + ".");
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+console.log("O livro " + livro + " foi publicado pela editora " + book( livro ).editora +".");
--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,7 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+var championship;
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,7 +12,7 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
+var teams = ["Santos", "Santo andre", "São Paulo" , "Bragatino-SP", "Mirassol"]
 
 console.log( 'Times que estão participando do campeonato:', teams );
 
